f8538bd3db6e2d88a7b31de00195f32efd9490b5
Kamil Soule <soulekamil@hotmail.com>
2017-05-20 21:53:17 +0200

    Fixed run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,6 @@
 #Restarts docker image with tests and starts test execution
 docker stop easyminer-web-ui-tests
 docker rm easyminer-web-ui-tests
-docker create --privileged --network=easyminer --name=easyminer-web-ui-tests -e ROBOT_TESTS=/Tests/ soulekamil/robot-docker
+docker create --privileged --network=easyminer --name=easyminer-web-ui-tests -e ROBOT_TESTS=/Tests/ soulekamil/robotframework-ff
 docker cp ./Tests/ easyminer-web-ui-tests:/Tests/
 docker start -i easyminer-web-ui-tests

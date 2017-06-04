1f7b56189e4c7cd19cf7e6c2d83a70fffc14d6f3
Kamil Soule <soulekamil@hotmail.com>
2017-06-04 17:30:30 +0200

    Ignore stdout during docker build

--- pull-easyminer-images-github.sh
+++ pull-easyminer-images-github.sh
@@ -3,6 +3,6 @@
 version=${1:-v2.4} # v2.4 is current stable latest
 branch=${2:-master}
 docker pull mariadb:10
-docker build -t kizi/easyminer-frontend:$version https://github.com/KIZI/EasyMiner-EasyMinerCenter.git#$branch:/
-docker pull kizi/easyminer-backend:$version  #todo change this pull to build from git repo once the Dockerfile is fixed
-docker build -t kizi/easyminer-scorer:$version https://github.com/KIZI/EasyMiner-Scorer.git#$branch:/
+docker build -t kizi/easyminer-frontend:$version https://github.com/KIZI/EasyMiner-EasyMinerCenter.git#$branch:/  1>/dev/null
+docker pull kizi/easyminer-backend:$version 1>/dev/null #todo change this pull to build from git repo once the Dockerfile is fixed  
+docker build -t kizi/easyminer-scorer:$version https://github.com/KIZI/EasyMiner-Scorer.git#$branch:/  1>/dev/null

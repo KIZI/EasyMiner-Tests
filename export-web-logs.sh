5dab4bb9dbf4f12279a69239be8ecd1dc894b4a9
Kamil Soule <soulekamil@hotmail.com>
2017-08-21 21:00:17 +0200

    Web export change.

--- export-web-logs.sh
+++ export-web-logs.sh
@@ -2,7 +2,6 @@
 exportToDirectory=./WebLogs
 mkdir -p $exportToDirectory
 docker cp easyminer-frontend:/var/www/html/easyminercenter/log /tmp/$TRAVIS_COMMIT
-zip -r $exportToDirectory/$TRAVIS_COMMIT.zip /tmp/$TRAVIS_COMMIT
-
-
+zip -r  /tmp/$TRAVIS_COMMIT.zip /tmp/$TRAVIS_COMMIT
+cp /tmp/$TRAVIS_COMMIT.zip $exportToDirectory/$TRAVIS_COMMIT.zip
 

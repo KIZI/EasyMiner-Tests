c5a7db79be832d17a8cdf2e29ab05dd5f7d9a12f
Kamil Soule <soulekamil@hotmail.com>
2017-08-21 21:33:51 +0200

    Export log file name change.

--- export-web-logs.sh
+++ export-web-logs.sh
@@ -2,6 +2,6 @@
 exportToDirectory=./WebLogs
 mkdir -p $exportToDirectory
 docker cp easyminer-frontend:/var/www/html/easyminercenter/log /tmp/$TRAVIS_COMMIT
-zip -r  /tmp/$TRAVIS_COMMIT.zip /tmp/$TRAVIS_COMMIT
-cp /tmp/$TRAVIS_COMMIT.zip $exportToDirectory/$TRAVIS_COMMIT.zip
+zip -r -j $exportToDirectory/EasyMiner-Web-Log-$TRAVIS_COMMIT.zip /tmp/$TRAVIS_COMMIT
+
 

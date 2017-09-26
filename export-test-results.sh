fb32a95a4597d53bec8df1f5e62a40ca5a66d46f
Kamil Soule <soulekamil@hotmail.com>
2017-09-26 19:06:51 +0200

    PDF export fix.

--- export-test-results.sh
+++ export-test-results.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 exportToDirectory=./TestResults/$TRAVIS_COMMIT
 mkdir -p $exportToDirectory
-docker cp easyminer-frontend:/TestResults/ $exportToDirectory
+cp -R ../TestResults/TestResults-*/* $exportToDirectory
